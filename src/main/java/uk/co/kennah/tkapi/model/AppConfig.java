f536b245119ba2ceb22d313a2b9d3e1a505165e7
TonyKennah <tonykennah@hotmail.com>
2025-08-09 15:14:40 +0100

    do not want to use Record.

--- src/main/java/uk/co/kennah/tkapi/model/AppConfig.java
+++ src/main/java/uk/co/kennah/tkapi/model/AppConfig.java
@@ -9,5 +9,32 @@ package uk.co.kennah.tkapi.model;
  * @param password The Betfair account password.
  * @param certPassword The password for the client certificate.
  */
-public record AppConfig(String appId, String username, String password, String certPassword) {
+public class AppConfig {
+    private final String appId;
+    private final String username;
+    private final String password;
+    private final String certPassword;
+
+    public AppConfig(String appId, String username, String password, String certPassword) {
+        this.appId = appId;
+        this.username = username;
+        this.password = password;
+        this.certPassword = certPassword;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCertPassword() {
+        return certPassword;
+    }
 }
